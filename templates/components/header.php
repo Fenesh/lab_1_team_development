13cdee2dafe6273caebceb133633f18fdd5b8358
Fenesh <fenesh.vanik@yandex.ru>
2024-12-03 12:08:26 +0300

    add basic style and components

---
+++ templates/components/header.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/index.php">Главная</a></li>
+            <li><a href="/pages/feed.html">Лента</a></li>
+            <li><a href="/pages/login.html">Авторизация</a></li>
+            <li><a href="/pages/register.html">Регистрация</a></li>
+            <li><a href="/pages/profile.html">Личный кабинет</a></li>
+        </ul>
+    </nav>
+</header>
